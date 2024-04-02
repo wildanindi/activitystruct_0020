19c4b84184fdeac386b7c46aa8ac742d2c8f33cc
wildanindi <wildanindi2@gmail.com>
2024-04-02 17:28:48 +0700

    di karenakan kita memakai struct di dalam struct kita juga harus menambahkan variabel pada fungsi cin untuk mengambil data pada struct dan pada fungsi cout untuk menampilkan data yang di masukkan

--- strukturA.cpp
+++ strukturA.cpp
@@ -19,14 +19,21 @@ int main(){
     cin >> mhs.nim;
     cout << "Nama Mahasiswa : ";
     cin >> mhs.nama;
-    cout << "Alamat Mahasiswa : ";
-    cin >> mhs.alamat;
+
+    cout << "Alamat Mahasiswa : " << endl;
+    cout << "\t Nama Desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "\t Nama Kota : ";
+    cin >> mhs.alamat.kota;
+
     cout << "Umur Mahasiswa : ";
     cin >> mhs.umur;
 
     cout << endl;
     cout << "\n NIM : "<<mhs.nim;
     cout << "\n Nama : "<<mhs.nama;
-    cout << "\n Alamat : "<<mhs.alamat;
+    cout << "\n Alamat : ";
+    cout << "\n Desa : " << mhs.alamat.desa;
+    cout << "\n kota : " << mhs.alamat.kota;
     cout << "\n Umur : "<<mhs.umur;
 }
