7e6c983cd34a5f94af610bae13b3c2fe8784772a
wildanindi <wildanindi2@gmail.com>
2024-04-02 17:50:28 +0700

    menambahkan fungsi yang sama untuk membacanya

--- strukturA.cpp
+++ strukturA.cpp
@@ -16,26 +16,28 @@ struct Mahasiswa {
 int main(){
     Mahasiswa mhs[3];
     for (int i=0;i<3,i++){
-    cout << "Nomor Mahasiswa : ";
-    getline(cin, mhs.nim);
-    cout << "Nama Mahasiswa : ";
-    getline(cin, mhs.nama);
+        cout << "Nomor Mahasiswa : ";
+        getline(cin, mhs.nim);
+        cout << "Nama Mahasiswa : ";
+        getline(cin, mhs.nama);
 
-    cout << "Alamat Mahasiswa : " << endl;
-    cout << "\t Nama Desa : ";
-    cin >> mhs.alamat.desa;
-    cout << "\t Nama Kota : ";
-    cin >> mhs.alamat.kota;
+        cout << "Alamat Mahasiswa : " << endl;
+        cout << "\t Nama Desa : ";
+        cin >> mhs.alamat.desa;
+        cout << "\t Nama Kota : ";
+        cin >> mhs.alamat.kota;
 
-    cout << "Umur Mahasiswa : ";
-    cin >> mhs.umur;
+        cout << "Umur Mahasiswa : ";
+        cin >> mhs.umur;
     }
 
-    cout << endl;
-    cout << "\n NIM : "<<mhs.nim;
-    cout << "\n Nama : "<<mhs.nama;
-    cout << "\n Alamat : ";
-    cout << "\n \t Desa : " << mhs.alamat.desa;
-    cout << "\n \t kota : " << mhs.alamat.kota;
-    cout << "\n Umur : "<<mhs.umur;
+    for (int i=0;i<3,i++){
+        cout << endl;
+        cout << "\n NIM : "<<mhs.nim;
+        cout << "\n Nama : "<<mhs.nama;
+        cout << "\n Alamat : ";
+        cout << "\n \t Desa : " << mhs.alamat.desa;
+        cout << "\n \t kota : " << mhs.alamat.kota;
+        cout << "\n Umur : "<<mhs.umur;
+    }
 }
